65649fcd181da883b95be7f959cea68ed144853f
Matthew D. Scholefield <matthew331199@gmail.com>
2020-02-17 10:46:40 -0600

    Increment version to 0.2.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='nuamo',
-    version='0.2.3',
+    version='0.2.4',
     description='Tool to generate potential project names',
     url='https://github.com/MatthewScholefield/nuamo',
     author='Matthew Scholefield',
